bc0a00d69f30573f1ef13dc187af2e587ebb104b
mdabusaeed <abu.saeed.nicl@gmail.com>
2025-02-22 09:25:30 +0600

    Initial commit

---
+++ Desktop/Saeed/Phitron/Semister 4/Software Development Project/Milstone 3/phimart/order/models.py
@@ -0,0 +1,67 @@
+from django.db import models
+from users.models import User
+from products.models import Product
+
+class Cart(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='cart')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.user.username}'
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.quantity} x {self.product.name}'
+
+    # def __str__(self):
+    #     return f'{self.product.name} - {self.quantity}'
+
+    def total(self):
+        return self.product.price * self.quantity
+    
+
+class Order(models.Model):
+    PENDING = 'Pending'
+    PROCESSING = 'Processing'
+    SHIPPED = 'Shipped'
+    DELIVERED = 'Delivered'
+    CANCELLED = 'Cancelled'
+    STATUS_CHOICES = [
+        (PENDING, 'Pending'),
+        (PROCESSING, 'Processing'),
+        (SHIPPED, 'Shipped'),
+        (DELIVERED, 'Delivered'),
+        (CANCELLED, 'Cancelled')
+    ]
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default=PENDING)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders')
+    items = models.ManyToManyField(CartItem)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)            
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f'Order #{self.id} by {self.user.username}'
+    
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='order_items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.quantity} x {self.product.name}'
+
+    def total(self):
+        return self.product.price * self.quantity
